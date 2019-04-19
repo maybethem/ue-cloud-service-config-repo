3ce4b259f2058638ad455ac46a3c5f9ca789ac07
liwenjing <563147097@qq.com>
2019-04-19 18:29:24 +0800

    Initial commit

---
+++ src/main/java/com/youedata/uecloudserviceconfigrepo/UeCloudServiceConfigRepoApplication.java
@@ -0,0 +1,13 @@
+package com.youedata.uecloudserviceconfigrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeCloudServiceConfigRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeCloudServiceConfigRepoApplication.class, args);
+	}
+
+}
